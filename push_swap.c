34796ee7efd7ee654aa6a42b2c96860efda41af7
Iker Trueba Escobal <itrueba-@c3r6s5.42urduliz.com>
2023-01-25 12:31:42 +0100

    Print

--- push_swap.c
+++ push_swap.c
@@ -6,37 +6,37 @@
 /*   By: itrueba- <itrueba-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:30:53 by itrueba-          #+#    #+#             */
-/*   Updated: 2023/01/25 12:23:06 by itrueba-         ###   ########.fr       */
+/*   Updated: 2023/01/25 12:31:12 by itrueba-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_print(t_push *push)
-{
-	while (push->stack_a || push->stack_b)
-	{
-		if (push->stack_a)
-		{
-			printf("%d -", push->stack_a->content);
-			push->stack_a = push->stack_a->next;
-		}
-		else
-		{
-			printf("  -");
-		}
-		if (push->stack_b)
-		{
-			printf(" %d", push->stack_b->content);
-			push->stack_b = push->stack_b->next;
-		}
-		printf("\n");
-	}
-	push->stack_a = push->head_a;
-	push->stack_b = push->head_b;
-	printf("A - B\n");
-	printf("%d - %d\n", push->size_a, push->size_b);
-}
+// void	ft_print(t_push *push)
+// {
+// 	while (push->stack_a || push->stack_b)
+// 	{
+// 		if (push->stack_a)
+// 		{
+// 			printf("%d -", push->stack_a->content);
+// 			push->stack_a = push->stack_a->next;
+// 		}
+// 		else
+// 		{
+// 			printf("  -");
+// 		}
+// 		if (push->stack_b)
+// 		{
+// 			printf(" %d", push->stack_b->content);
+// 			push->stack_b = push->stack_b->next;
+// 		}
+// 		printf("\n");
+// 	}
+// 	push->stack_a = push->head_a;
+// 	push->stack_b = push->head_b;
+// 	printf("A - B\n");
+// 	printf("%d - %d\n", push->size_a, push->size_b);
+// }
 
 void	ft_correct(t_push *push)
 {
